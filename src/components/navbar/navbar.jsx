46249c36969acf7a37d5caecf62742dbf345c8a8
re-andrade1987 <renataandrade1987@hotmail.com>
2023-03-09 17:29:29 -0300

    Criando nossas primeiras páginas e componentes

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,15 @@
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>Sobre o projeto</li>
+        <li>Conheça nosso trabalho</li>
+        <li>Se inscreva nas nossas atividades</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/monteKemelPage/monteKemelPage.jsx
@@ -0,0 +1,15 @@
+import './monteKemelPage.css';
+
+function MonteKemelPage() {
+  return (
+    <div className="monteKemelPage">
+      <header className="App-header">
+        <p>
+          Essa vai ser a página da instituição
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default MonteKemelPage;
