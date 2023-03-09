845d95fe88099c1e24c90a013635122aa2a03cd2
re-andrade1987 <renataandrade1987@hotmail.com>
2023-03-09 17:42:52 -0300

    mudanças na navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,6 +3,7 @@ import './navbar.css';
 function Navbar() {
   return (
     <div className="navbar">
+        <div className="navbarLogo">Logo aqui</div>
       <ul>
         <li>Sobre o projeto</li>
         <li>Conheça nosso trabalho</li>
